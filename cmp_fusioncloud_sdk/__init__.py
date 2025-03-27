504bbb84d03635725e6c1899d04d2d9c4767de93
xjbzzz <2907321961@qq.com>
2025-03-27 13:51:41 +0800

    添加cmp_fusioncloud_sdk __init__.py文件

---
+++ cmp_fusioncloud_sdk/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+CMP Fusion Cloud SDK
+~~~~~~~~~~~~~~~~~~~
+
+华为云Fusion Cloud管理平台的SDK封装。
+
+:copyright: (c) 2025
+:license: Private
+"""
+
+__version__ = '1.0.0'
+
+from cmp_fusioncloud_sdk.operate_class import CmpFusionCloudSDK
+
+__all__ = ['CmpFusionCloudSDK']
